75e097b66d4144837068e541f6aef59e2dbf11fa
holasmart <holasmart>
2026-06-25 09:07:23 +0800

    first commit

---
+++ main.ts
@@ -0,0 +1,12 @@
+/**
+ * Control keyestudio DeskBitCar
+ * author: xiuxian
+ * github:https://github.com/Ungravitation/DeskBit
+ * Write the date: 2021-01-05
+ */
+
+//% color="#ff6800" icon="\uf1b9" 
+//% groups='["Car", "Servo", "Configuration"]'
+namespace IoT_keyes {
+
+}
---
+++ targetOverrides.ts
@@ -0,0 +1,8 @@
+namespace servos {
+    //% block="servo P0" fixedInstance whenUsed
+    export const P0 = new IoT_keyes.PinServo(pins.P0);
+    //% block="servo P1" fixedInstance whenUsed
+    export const P1 = new IoT_keyes.PinServo(pins.P1);
+    //% block="servo P2" fixedInstance whenUsed
+    export const P2 = new IoT_keyes.PinServo(pins.P2);
+}
